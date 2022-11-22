633f2d91c2223cffeb3cb18d94a72bc3fd43b3c3
K.Y <orangenspice@gmail.com>
2022-11-23 02:34:32 +1100

    fix 3

--- backend/project/Project.cs
+++ backend/project/Project.cs
@@ -165,26 +165,29 @@ namespace TradingBot
                 return;
 
             var chand = candle.chandDecision;
-            var macd = candle.macd;
-            var sig = candle.signal;
+            decimal macd = 0;
+            decimal sig = 0;
             var prevCross = "-";
             for (int i = data.Count() - 1; i >= 0; --i)
             {
-                if (!candle.cross.Equals("-") && prevCross.Equals("-"))
+                if (!data[i].cross.Equals("-") && prevCross.Equals("-"))
                 {
-                    prevCross = candle.cross;
-                    continue;
-                }
-            }
+                    prevCross = data[i].cross;
+                    macd = data[i].macd != null ? (decimal)data[i].macd! : 0;
+                    sig = data[i].signal != null ? (decimal)data[i].signal! : 0;
 
-            if ((macd < 0 || sig < 0) && (prevCross.Equals("green")) && (chand.Equals("buy")))
-                candle.finalDecision = "buy";
-            else if ((prevCross.Equals("red") && macd < 0) || (chand.Equals("sell")))
-                candle.finalDecision = "sell";
-            else if ((macd > 0 || sig > 0) && (prevCross.Equals("red")) && chand.Equals("sell"))
-                candle.finalDecision = "buy";
-            else if ((prevCross.Equals("green") && macd > 0) || chand.Equals("buy"))
-                candle.finalDecision = "sell";
+                    if ((macd < 0 || sig < 0) && (prevCross.Equals("green")) && (chand.Equals("buy")))
+                        candle.finalDecision = "buy";
+                    else if ((prevCross.Equals("red") && macd < 0) || (chand.Equals("sell")))
+                        candle.finalDecision = "sell";
+                    else if ((macd > 0 || sig > 0) && (prevCross.Equals("red")) && chand.Equals("sell"))
+                        candle.finalDecision = "buy";
+                    else if ((prevCross.Equals("green") && macd > 0) || chand.Equals("buy"))
+                        candle.finalDecision = "sell";
+
+                    return;
+                }
+            }            
         }
 
 
